e14c04a00e92ccc8600bff32d9928d46a8379ebd
ARandomLemon <c00301121@setu.ie>
2023-11-09 23:51:28 +0000

    odd numbers are cool

--- numbers/numbers/Source.cpp
+++ numbers/numbers/Source.cpp
@@ -1,7 +1,18 @@
 // odd Tymek
+// even Tymek
+
+#include <iostream>
 
 int main()
 {
+	int input = 7; //user inputted number
 
+	std::cout << "think of a number:" << std::endl;
+	std::cin >> input;
+
+	if (input % 2 == 1)
+	{
+		std::cout << "the number was odd" << std::endl;
+	}
 	return 1;
 }
