c05bf96bc8b4ffacbce0a85635dbf39feb901f44
ARandomLemon <c00301121@setu.ie>
2023-11-09 23:56:51 +0000

    even numbers

--- numbers/numbers/Source.cpp
+++ numbers/numbers/Source.cpp
@@ -1,7 +1,16 @@
 // odd Tymek
 
+#include <iostream>
 int main()
 {
+	int number = 8; //user number
 
+	std::cout << " what is your number :" << std::endl;
+	std::cin >> number;
+
+	if (number % 2 == 0)
+	{
+		std::cout << "the number was even" << std::endl;
+	}
 	return 1;
 }
